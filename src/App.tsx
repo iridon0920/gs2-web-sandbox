4750754c3289e7dc8566e75ca408a38d272b4c5c
iridon0920 <iridon0920@gmail.com>
2024-09-29 23:56:48 +0900

    add: 認証用関数経由でのトークン取得、それを使ったGs2AccountのNamespaceリスト出力

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios, { AxiosRequestConfig } from "axios";
+import { Gs2AccountRestClient } from "gs2/account";
+import { DescribeNamespacesRequest } from "gs2/account/request";
+import { Gs2RestSession, ProjectTokenGs2Credential } from "gs2/core/model";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [namespaces, setNamespaces] = useState<(string | null)[]>([]);
+
+  useEffect(() => {
+    const initClient = async () => {
+      const clientId = process.env.REACT_APP_GS2_CLIENT_ID as string;
+      const region = "ap-northeast-1";
+
+      const config: AxiosRequestConfig = {
+        url: process.env.REACT_APP_GET_GS2_TOKEN_URL,
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: { clientId, region },
+      };
+      const result = await axios.request(config);
+      const session = new Gs2RestSession(
+        new ProjectTokenGs2Credential(clientId, result.data.projectToken),
+        region,
+      );
+      await session.connect();
+
+      const gs2Account = new Gs2AccountRestClient(session);
+      const namespacesResult = await gs2Account.describeNamespaces(
+        new DescribeNamespacesRequest().withLimit(10),
+      );
+
+      const fetchedNamespaces = namespacesResult.getItems();
+      if (fetchedNamespaces) {
+        setNamespaces(
+          fetchedNamespaces.map((namespace) => namespace.getName()),
+        );
+      }
+    };
+    initClient().catch((err) => console.log(err));
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Namespaces</h1>
+      <ul>
+        {namespaces.map((namespace, index) => (
+          <li key={index}>{namespace}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
